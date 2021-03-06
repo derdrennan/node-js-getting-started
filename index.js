e8ad5a865006a6eafb6c337ae10899b813a47996
Derek Drennan <derekay@live.com>
2021-03-06 11:07:51 -0800

    added comments

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const PORT = process.env.PORT || 5000;
 
 // tell it to use the public directory as one where static files live
 app.use(express.static(__dirname + '/public'));
+//app.use(express.static(__dirname + '/javascripts'));
   
 // views is directory for all template files
 app.set('views', path.join(__dirname + '/views'));
--- public/javascripts/calculateRate.js
+++ public/javascripts/calculateRate.js
@@ -1,63 +1,62 @@
 function setPostageParams(request, response) {
-  const weight = request.query.weight;
-  const mailType = request.query.mailType;
-
-  calculatePostageRate(response, weight, mailType);
-}
-
-function calculatePostageRate(response, weight, mailType) {
+    var weight = request.query.weight;
+    var mailType = request.query.mailType;
+    var price = 0;
   
-  const price = 0;
-
-  if (mailType == 'Letter (Stamped)') {
-    if (weight <= 1) {
-        price = 0.55;
-    } else if (weight <= 2) {
-        price = 0.75;
-    } else if (weight <= 3) {
-        price = 0.95;
-    } else if (weight <= 3.5) {
-        price = 1.15;
-    }
+    calculatePostageRate(response, weight, mailType, price);
   }
-
-  if (mailType == 'Letter (Metered)') {
-    if (weight <= 1) {
-        price = 0.51;
-    } else if (weight <= 2) {
-        price = 0.71;
-    } else if (weight <= 3) {
-        price = 0.91;
-    } else if (weight <= 3.5) {
-        price = 1.11;
+  
+  function calculatePostageRate(response, weight, mailType, price) {
+  
+    if (mailType == 'Letter (Stamped)') {
+      if (weight <= 1) {
+          price = 0.55;
+      } else if (weight <= 2) {
+          price = 0.75;
+      } else if (weight <= 3) {
+          price = 0.95;
+      } else if (weight <= 3.5) {
+          price = 1.15;
+      }
     }
-  }
-
-  if (mailType == 'Large Envelope (Flat)') {
-    if (weight <= 1) {
-        price = 1.00;
-    } else if (weight <= 2) {
-        price = 1.20;
-    } else if (weight <= 3) {
-        price = 1.40;
-    } else if (weight <= 4) {
-        price = 1.60;
+  
+    if (mailType == 'Letter (Metered)') {
+      if (weight <= 1) {
+          price = 0.51;
+      } else if (weight <= 2) {
+          price = 0.71;
+      } else if (weight <= 3) {
+          price = 0.91;
+      } else if (weight <= 3.5) {
+          price = 1.11;
+      }
     }
-  }
-
-  if (mailType == 'First-Class Package Service - Retail') {
-    if (weight <= 1) {
-        price = 4.00;
-    } else if (weight <= 2) {
-        price = 4.00;
-    } else if (weight <= 3) {
-        price = 4.00;
-    } else if (weight <= 4) {
-        price = 4.00;
+  
+    if (mailType == 'Large Envelope (Flat)') {
+      if (weight <= 1) {
+          price = 1.00;
+      } else if (weight <= 2) {
+          price = 1.20;
+      } else if (weight <= 3) {
+          price = 1.40;
+      } else if (weight <= 4) {
+          price = 1.60;
+      }
     }
+  
+    if (mailType == 'First-Class Package Service - Retail') {
+      if (weight <= 1) {
+          price = 4.00;
+      } else if (weight <= 2) {
+          price = 4.00;
+      } else if (weight <= 3) {
+          price = 4.00;
+      } else if (weight <= 4) {
+          price = 4.00;
+      }
+    }
+  
+    const params = { weight: weight, mailType: mailType, price: price };
+  
+    response.render('pages/result', params);
   }
-
-  const params = { weight: weight, mailType: mailType, price: price };
-
-  response.render('pages/result', params);
-}
